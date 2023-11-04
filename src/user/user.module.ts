0acc2ddec27b37a2be9f8c653ace73f3b2b1a2c8
salehziad <salehziad1999@gmail.com>
2023-11-04 21:08:40 +0300

    Remove unused code

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,10 +1,6 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
-import { PrismaService } from '../prisma.service';
-
-import { JwtService } from '@nestjs/jwt';
-import { RequestService } from '../request.service';
 import { SharedModule } from 'src/shared/shared.module';
 
 @Module({
